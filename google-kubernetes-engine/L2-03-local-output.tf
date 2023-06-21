ddaab7a47ff160019222376c535e2cc85b70439e
billy <wachun.git@gmail.com>
2023-06-21 12:22:59 +0800

    Add L2-03-local-output.tf v3

--- google-kubernetes-engine/L2-03-local-output.tf
+++ google-kubernetes-engine/L2-03-local-output.tf
@@ -9,22 +9,19 @@ output "generic_project_id" {
 # GCP Region
 output "generic_gcp_region" {
   description = "Region in which GKE Resources to be created"
-  type        = string
-  default     = var.gcp_region
+  default = var.gcp_region
 }
 
 # Environment Variable
 output "generic_environment" {
   description = "Environment Variable as a suffix"
-  type        = string
-  default     = var.environment
+  default = var.environment
 }
 
 # Product Name
 output "generic_product_name" {
   description = "Product Name"
-  type        = string
-  default     = var.product_name
+  default = var.product_name
 }
 
 # Local Output Values
@@ -32,6 +29,5 @@ output "generic_product_name" {
 # local variable ('name')
 output "local_name" {
   description = "locals name"
-  type        = string
-  default     = local.name
+  default = local.name
 }

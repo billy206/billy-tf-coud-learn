d84fcd666557e822f511e2a935ae2cf9554d9b42
billy <wachun.git@gmail.com>
2023-06-23 03:08:27 +0800

    test 116

--- billy-mongod-atlas-gcp-vpc-peering-terraform/M2-mongodb-atlas-network-peering-resource.tf
+++ billy-mongod-atlas-gcp-vpc-peering-terraform/M2-mongodb-atlas-network-peering-resource.tf
@@ -23,3 +23,15 @@ resource "mongodbatlas_network_peering" "mongodb-atlas-peering" {
   gcp_project_id = data.terraform_remote_state.workspace.outputs.generic_project_id
   network_name   = data.terraform_remote_state.workspace.outputs.network_name
 }
+
+# Resource - Create Private Endpoint of MongoDB Atlas Network
+resource "mongodbatlas_privatelink_endpoint" "mongodb-atlas-private_endpoint" {
+  # MongoDB Atlas Project ID
+  project_id = var.mongodb_atlas_project_id
+
+  # MongoDB Atlas Provider Name
+  provider_name = var.mongodb_atlas_provider_name
+
+  # MongoDB Atlas Region
+  region = var.provider_region_name
+}
